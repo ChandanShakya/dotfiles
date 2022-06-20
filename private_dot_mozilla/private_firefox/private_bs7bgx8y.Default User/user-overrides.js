0d9095bd386c5ec9c896f3812e14fe5ce9f33270
ZXY-CC-3ag13 <zxy-cc-3ag13@protonmail.com>
2022-06-20 18:57:47 +0545

    Style: Add comments

--- private_dot_mozilla/private_firefox/private_bs7bgx8y.Default User/user-overrides.js
+++ private_dot_mozilla/private_firefox/private_bs7bgx8y.Default User/user-overrides.js
@@ -1,10 +1,36 @@
+// For DNS
 user_pref("network.trr.mode", 3);
+
+// Total Cookie Protection On
 user_pref("network.cookie.cookieBehavior", 5);
-user_pref("browser.compactmode.show", "true");
-user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", "false");
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", "false");
-user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", "false");
-user_pref("browser.newtabpage.activity-stream.showSponsored", "false");
-user_pref("extensions.pocket.enabled", "false");
-user_pref("media.peerconnection.enabled", "false");
-user_pref("media.navigator.enabled", "false");
+
+// Enable Compact mode
+user_pref("browser.compactmode.show", true);
+
+// Disable Pocket
+user_pref("extensions.pocket.enabled", false);
+
+// Disable WebRTC
+user_pref("media.peerconnection.enabled", false);
+
+// Set Startup Page
+user_pref("browser.startup.page", 1);
+user_pref("browser.startup.homepage", "moz-extension://12466af4-4786-4a87-8df6-c6a2bce2c269/reader.html");
+
+// Address Bar Suggestions
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+// Disable Password Saving
+user_pref("signon.rememberSignons", false);
+
+// Disable permissions
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.camera", 2);
+user_pref("permissions.default.microphone", 2);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.xr", 2);
+
+// Disable document fonts
+user_pref("browser.display.use_document_fonts", 0);
+
